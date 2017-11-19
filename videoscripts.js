248a2a0b5d4ed62c716bf5af767fe6a5e8b94c7e
Sospsig <sos42@hi.is>
2017-11-19 18:37:55 +0000

    Sigga commit

--- videoscripts.js
+++ videoscripts.js
@@ -50,6 +50,10 @@ function video(data) {
     section1.appendChild(headtext);
 
   var sectionvid = document.createElement("section");
+  sectionvid.setAttribute("class", "sectionvid");
+
+
+// Video
   var video = document.createElement("video");
   video.setAttribute("class","video");
   document.body.appendChild(video);
@@ -58,136 +62,151 @@ function video(data) {
   document.querySelector("main").appendChild(sectionvid);
   sectionvid.appendChild(video);
 
-// takkar
-var sectbutt = document.createElement("section");
-var divbtn = document.createElement("div");
-sectbutt.appendChild(divbtn);
 
-document.querySelector("main").appendChild(sectbutt);
-/* Rewind takki*/
-var rwndbtn = document.createElement('button');
-rwndbtn.setAttribute('class','takkar');
-var rwnd = document.createElement('img');
-rwnd.src = ('img/back.svg');
-rwndbtn.appendChild(rwnd);
-divbtn.appendChild(rwndbtn);
-rwndbtn.addEventListener('click', function () {
-    video.currentTime -= 3;
+// Pic overlay
+  var picover = document.createElement('cover');
+  var pic = document.createElement('img');
+  pic.src = data.poster;
+  picover.appendChild(pic);
+  sectionvid.appendChild(picover);
 
-});
 
-// Play takki
-var playbtn = document.createElement('button');
-playbtn.setAttribute('class','takkar');
-var play = document.createElement('img');
-play.src = ('img/play.svg')
-playbtn.appendChild(play);
-divbtn.appendChild(playbtn);
+  // takkar
+  var sectbutt = document.createElement("section");
+  sectbutt.setAttribute("class", "sectbutt");
 
-playbtn.addEventListener('click', function () {
-  if (video.paused) {
-    video.play();
-    playbtn.removeChild(playbtn.firstChild);
-    var pause = document.createElement('img');
-    pause.src = ('img/pause.svg')
-    playbtn.appendChild(pause);
-  } else {
-    video.pause();
-    playbtn.removeChild(playbtn.firstChild);
-    var play = document.createElement('img');
-    play.src = ('img/play.svg')
-    playbtn.appendChild(play);
-  }
-});
+  document.querySelector("main").appendChild(sectbutt);
+  /* Rewind takki*/
+  var rwndbtn = document.createElement('button');
+  rwndbtn.setAttribute('class','takkar');
+  var rwnd = document.createElement('img');
+  rwnd.src = ('img/back.svg');
+  rwndbtn.appendChild(rwnd);
+  sectbutt.appendChild(rwndbtn);
+  rwndbtn.addEventListener('click', function () {
+      video.currentTime -= 3;
 
-// Mute takki
-var mutebtn = document.createElement('button');
-mutebtn.setAttribute('class','takkar');
-var mute = document.createElement('img');
-mute.src = ('img/mute.svg')
-mutebtn.appendChild(mute);
-divbtn.appendChild(mutebtn);
+  });
 
-mutebtn.addEventListener('click', function () {
-  if (video.muted == false) {
-    video.muted = true;
-    mutebtn.removeChild(mutebtn.firstChild);
-    var unmute = document.createElement('img');
-    unmute.src = ('img/unmute.svg')
-    mutebtn.appendChild(unmute);
-  } else {
-    video.muted = false;
-    mutebtn.removeChild(mutebtn.firstChild);
-    var mute = document.createElement('img');
-    mute.src = ('img/mute.svg')
-    mutebtn.appendChild(mute);
-  }
-});
+  // Play takki
+  var playbtn = document.createElement('button');
+  playbtn.setAttribute('class','takkar');
+  var play = document.createElement('img');
+  play.src = ('img/play.svg')
+  playbtn.appendChild(play);
+  sectbutt.appendChild(playbtn);
 
-// Fullscreen
-var fullbtn = document.createElement('button');
-fullbtn.setAttribute('class','takkar');
-var fullscr = document.createElement('img');
-fullscr.src = ('img/fullscreen.svg')
-fullbtn.appendChild(fullscr);
-divbtn.appendChild(fullbtn);
+  // play takki, overlay
+  var overbtn = document.createElement('button');
+  var over = document.createElement('img');
+  overbtn.setAttribute('class','vidbtn');
+  sectionvid.appendChild(overbtn);
+  over.src = ('img/play.svg')
+  overbtn.appendChild(over);
 
-fullbtn.addEventListener('click', function () {
-  var elem = document.getElementById("video");
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-  } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-  } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
+
+  playbtn.addEventListener('click', function () {
+    if (video.paused) {
+      video.play();
+      playbtn.removeChild(playbtn.firstChild);
+      overbtn.removeChild(overbtn.firstChild);
+      picover.removeChild(picover.firstChild);
+      var pause = document.createElement('img');
+      pause.src = ('img/pause.svg')
+      playbtn.appendChild(pause);
+    } else {
+      video.pause();
+      playbtn.removeChild(playbtn.firstChild);
+      var play = document.createElement('img');
+      play.src = ('img/play.svg')
+      playbtn.appendChild(play);
+
+      var over = document.createElement('img');
+      overbtn.setAttribute('class','vidbtn');
+      sectionvid.appendChild(overbtn);
+      over.src = ('img/play.svg')
+      overbtn.appendChild(over);
     }
-});
+  });
 
-// Rewind takki
-var nextbtn = document.createElement('button');
-nextbtn.setAttribute('class','takkar');
-var next = document.createElement('img');
-next.src = ('img/next.svg')
-nextbtn.appendChild(next);
-divbtn.appendChild(nextbtn);
+  // play takki, overlay process
+  overbtn.addEventListener('click', function () {
+      overbtn.removeChild(overbtn.firstChild);
+      video.play();
 
-nextbtn.addEventListener('click', function () {
-    video.currentTime += 3;
-});
+      playbtn.removeChild(playbtn.firstChild);
+      picover.removeChild(picover.firstChild);
+      var pause = document.createElement('img');
+      pause.src = ('img/pause.svg')
+      playbtn.appendChild(pause);
+  });
 
+  // Mute takki
+  var mutebtn = document.createElement('button');
+  mutebtn.setAttribute('class','takkar');
+  var mute = document.createElement('img');
+  mute.src = ('img/mute.svg')
+  mutebtn.appendChild(mute);
+  sectbutt.appendChild(mutebtn);
 
-// play takki, overlay
-var overbtn = document.createElement('button');
-var over = document.createElement('img');
+  mutebtn.addEventListener('click', function () {
+    if (video.muted == false) {
+      video.muted = true;
+      mutebtn.removeChild(mutebtn.firstChild);
+      var unmute = document.createElement('img');
+      unmute.src = ('img/unmute.svg')
+      mutebtn.appendChild(unmute);
+    } else {
+      video.muted = false;
+      mutebtn.removeChild(mutebtn.firstChild);
+      var mute = document.createElement('img');
+      mute.src = ('img/mute.svg')
+      mutebtn.appendChild(mute);
+    }
+  });
 
-overbtn.setAttribute('class','vidbtn');
-sectionvid.appendChild(overbtn);
-over.src = ('img/play.svg')
-overbtn.appendChild(over);
+  // Fullscreen
+  var fullbtn = document.createElement('button');
+  fullbtn.setAttribute('class','takkar');
+  var fullscr = document.createElement('img');
+  fullscr.src = ('img/fullscreen.svg')
+  fullbtn.appendChild(fullscr);
+  sectbutt.appendChild(fullbtn);
 
-overbtn.addEventListener('click', function () {
-  overbtn.removeChild(overbtn.firstChild);
-  video.play();
+  fullbtn.addEventListener('click', function () {
+    var elem = document.getElementById("video");
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
+      }
+  });
 
-  playbtn.removeChild(playbtn.firstChild);
-  var pause = document.createElement('img');
-  pause.src = ('img/pause.svg')
-  playbtn.appendChild(pause);
-});
+  // Rewind takki
+  var nextbtn = document.createElement('button');
+  nextbtn.setAttribute('class','takkar');
+  var next = document.createElement('img');
+  next.src = ('img/next.svg')
+  nextbtn.appendChild(next);
+  sectbutt.appendChild(nextbtn);
 
+  nextbtn.addEventListener('click', function () {
+      video.currentTime += 3;
+  });
+  var footer1 = document.createElement("section");
+  footer1.setAttribute("class", "footer");
+  footer1.setAttribute("style", "width:1200px;");
+  document.querySelector("main").appendChild(footer1);
 
-var footer1 = document.createElement("section");
-footer1.setAttribute("class", "footer");
-footer1.setAttribute("style", "width:1200px;");
-document.querySelector("main").appendChild(footer1);
-
-var foottext = document.createElement("p");
-var text = document.createTextNode("Til Baka");
-foottext.setAttribute("style", "padding-bottom:10px;");
-foottext.appendChild(text);
-footer1.appendChild(foottext);
-
+  var foottext = document.createElement("p");
+  var text = document.createTextNode("Til Baka");
+  foottext.setAttribute("style", "padding-bottom:10px;");
+  foottext.appendChild(text);
+  footer1.appendChild(foottext);
 }
+
 }
 
 allt();
